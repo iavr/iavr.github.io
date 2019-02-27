af1ba1dc76d23590388e2c49e1dac3084dcff9e7
iavr <iavr@users.noreply.github.com>
2019-02-27 17:38:33 +0100

    add content

---
+++ web/js/email.js
@@ -0,0 +1,20 @@
+function ema(addr, sty) {
+	var href = "mail" + "to:" + addr;
+	document.write("<a class=\"" + sty + "\" href=" + href + ">");
+}
+function txt(msg) {
+	document.write(msg);
+}
+function yann() {
+	var first = "yannis";
+	var last = "avrithis";
+	var dom = "net";
+	return first + "@" + last + "." + dom;
+}
+function ioan(sty) {
+	var first = "ioannis";
+	var last = "avrithis";
+	var dom = "inria";
+	var fr = "fr";
+	return first + "." + last + "@" + dom + "." + fr;
+}
