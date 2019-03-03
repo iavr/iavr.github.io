2fd08190d5458cc0261b808fc32f7d9cb302ad8e
iavr <iavr@users.noreply.github.com>
2019-03-03 01:44:04 +0100

    nav

---
+++ web/js/scroll.js
@@ -0,0 +1,36 @@
+function view() {
+	var e = window, a = 'inner';
+	if(!('innerWidth' in window)) {
+		e = document.documentElement || document.body;
+		a = 'client';
+	}
+	return {width: e[a+'Width'], height: e[a+'Height']}
+}
+
+function scroll(h) {
+	var e = view().width >= 768 ? 'header' : 'nav';
+	var height = $(e).outerHeight(true);
+	var margin = parseInt($(h).parent().css('margin-top'));
+	var off = $(h).offset().top - margin - height + 100;
+	$('html, body').animate({scrollTop: off}, 500);
+}
+
+function int_hash(e) {
+	scroll($(e).attr('href'));
+}
+
+function ext_hash() {
+	var h = window.location.hash;
+	if(h && !h.includes('#col-')) {
+		if(!h.includes('#y-')) {
+			var e = $('#col-' + h.slice(1));
+			if(e) e.addClass('show');
+		}
+		scroll(h);
+	}
+}
+
+$(document).ready(function() {
+	$("a[href^='#']").on('click', function() {int_hash(this);});
+	ext_hash();
+});
